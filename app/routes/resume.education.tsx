03c956c5b053b0793f48d968789e4d218f6e8acd
Shakeel Haider <shkhaider2015@gmail.com>
2025-01-05 18:56:53 +0500

    education done

--- app/routes/resume.education.tsx
+++ app/routes/resume.education.tsx
@@ -1,7 +1,35 @@
+import EducationItem from "components/EducationItem/EducationItem";
+
 const Education = () => {
   return (
     <div className="tab-screen-con">
-      <h1>Education</h1>
+      <p className="descriptiion education-desc" >
+        Lorem ipsum dollar sit amet Lorem ipsum dollar sit amet Lorem ipsum
+        dollar sit amet Lorem ipsum dollar sit amet Lorem ipsum dollar sit amet
+        Lorem ipsum dollar sit amet{" "}
+      </p>
+      <div className="education-items">
+        <EducationItem
+          date="2019 - 2022"
+          title="Full Stack Developer"
+          companyName="Retrocue"
+        />
+        <EducationItem
+          date="2019 - 2022"
+          title="Full Stack Developer"
+          companyName="Retrocue"
+        />
+        <EducationItem
+          date="2019 - 2022"
+          title="Full Stack Developer"
+          companyName="Retrocue"
+        />
+        <EducationItem
+          date="2019 - 2022"
+          title="Full Stack Developer"
+          companyName="Retrocue"
+        />
+      </div>
     </div>
   );
 };
---
+++ components/EducationItem/EducationItem.tsx
@@ -0,0 +1,21 @@
+import { IExperienceItem } from 'utils/interfaces/components';
+import './EducationItem.css'
+
+const EducationItem = (props:IExperienceItem) => {
+  const { date, title, companyName } = props;
+
+  return <div className="experience-item-con">
+    <div className="content-con">
+      <div className="ribbon" >some</div>
+        <h4 className="date">{date}</h4>
+        <p className="title">{title}</p>
+        <div className='company' >
+            <div className='circle' />
+            <span className='title' >{companyName}</span>
+        </div>
+        {/* <div className='left-tringle' /> */}
+      </div>
+  </div>;
+};
+
+export default EducationItem;
