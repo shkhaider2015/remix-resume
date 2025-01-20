bcec986c9bc4a8224e0b03077b16cae1be490eae
Shakeel Haider <shkhaider2015@gmail.com>
2025-01-20 19:20:16 +0500

    update config

--- vite.config.ts
+++ vite.config.ts
@@ -3,8 +3,10 @@ import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 
 export default defineConfig({
+  base: 'remix-resume',
   plugins: [
     remix({
+      basename: 'remix-resume',
       ignoredRouteFiles: ["**/*.css"],
     }),
     tsconfigPaths(),
