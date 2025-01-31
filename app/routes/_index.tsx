095165f44231dd62e20a18af92ab40ec978327bf
Shakeel Haider <shkhaider2015@gmail.com>
2025-01-31 17:55:22 +0500

    added bacground

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,10 +16,10 @@ export default function Index() {
     <div className="home-container">
       <div className="home-section home-left">
         <h1>Shakeel Haider</h1>
-        <h3>Software Engineer</h3>
+        <h3>Software Engineer 👨🏻‍💻</h3>
         <p>
           I'm a passionate software engineer with over 5 years of experience in
-          frontend, backend, and database development. Skilled in creating
+          🔴 frontend, 🟢 backend, and 🔵 database development. Skilled in creating
           user-friendly interfaces, scalable applications, and efficient data
           solutions, I thrive on solving complex problems and delivering
           high-quality software.
