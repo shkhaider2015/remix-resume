e04e0ee0290b851a60581940f805f84acc6aef12
Shakeel Haider <shkhaider2015@gmail.com>
2025-04-16 23:17:19 +0500

    issue resolved

--- app/assets/images.ts
+++ app/assets/images.ts
@@ -9,5 +9,4 @@ export { default as MobApps } from '~/assets/images/smartphone.png';
 export { default as UIUX } from '~/assets/images/ux-design.png';
 export { default as DevOps } from '~/assets/images/devops.png';
 export { default as ProfilePictre } from '~/assets/images/profile-picture.png';
-export { default as ProfilePictre2 } from '~/assets/images/profile-picture-ghibli.png';
 export { default as ProfilePictre3 } from '~/assets/images/profile-picture-ghibli-2.png';
