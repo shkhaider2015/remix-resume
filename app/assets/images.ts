827449845462bc3efe72ab36883b25cf294962cb
Shakeel Haider <shkhaider2015@gmail.com>
2025-06-18 20:59:51 +0500

    change profile picture

--- app/assets/images.ts
+++ app/assets/images.ts
@@ -8,6 +8,5 @@ export { default as WebApps } from '~/assets/images/website.png';
 export { default as MobApps } from '~/assets/images/smartphone.png';
 export { default as UIUX } from '~/assets/images/ux-design.png';
 export { default as DevOps } from '~/assets/images/devops.png';
-export { default as ProfilePictre } from '~/assets/images/profile-picture.png';
-export { default as ProfilePictre3 } from '~/assets/images/profile-picture-ghibli-2.png';
+export { default as ProfilePictre } from '~/assets/images/newDP.png';
 export { default as PDFLogo } from '~/assets/images/pdf.png';
