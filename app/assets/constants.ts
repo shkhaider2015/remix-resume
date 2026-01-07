80a0799aa08459c15ad4d14a5c841ce330698012
Shakeel Haider <shkhaider2015@gmail.com>
2026-01-07 16:33:16 +0500

    translation added to public as well

--- app/assets/constants.ts
+++ app/assets/constants.ts
@@ -2,4 +2,4 @@ export const BASE_URL = "https://www.shakeelhaider.com";
 
 export type SupportedLocale = "ar" | "de" | "el" | "en" | "es" | "fr" | "it" | "ko" | "pl" | "pt" | "ru" | "sv" | "ur" | "zh"
 export const DEFAULT_LOCALE: SupportedLocale = "en";
-export const SUPPORTED_LOCALES: SupportedLocale[] = ["ar", "de", "el", "en", "es", "fr", "it", "ko", "pl", "pt", "ru", "sv"];
+export const SUPPORTED_LOCALES: SupportedLocale[] = ["ar", "de", "el", "en", "es", "fr", "it", "ko", "pl", "pt", "ru", "sv", "ur", "zh"];
